ffcee733b8aa2bec1f312141240a56f0cf462ab6
Naveen <naveenuusa@gmail.com>
2023-02-28 16:06:37 +0530

    patched python file

--- Python.py
+++ Python.py
@@ -1,8 +1,8 @@
 # Define the main function to perform arithmetic operations
 def calculator():
     # Prompt user for input and convert it to a float
-    num1 = float(input("Enter the first alphabet: "))
-    num2 = float(input("Enter the second number: "))
+    num1 = float(input("Enter the first number: "))
+    num2 = float(input("Enter the 2nd number: "))
 
     # Prompt user for operation
     operation = input("Enter the operation (+, -, *, /): ")
